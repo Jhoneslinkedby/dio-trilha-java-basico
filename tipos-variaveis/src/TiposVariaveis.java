2eeb391e886401e97cce5143b9f259aa21e48968
Jhones Correa - Linkedby <jhones.correa@controlinformatica.com.br>
2024-05-23 17:13:42 -0300

    update project

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+    short salario = (short) 2;
+
+    final String BR = "Brasil";
+
+        System.out.println(salario);
+    }
+}
